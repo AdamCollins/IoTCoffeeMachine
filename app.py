7d3f85ab97d14327e538b82c6b8ccc89bd34cf38
Grace Lam <gracesmile88@hotmail.com>
2019-01-26 19:03:33 -0800

    Working code for pulling status updates

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ lib = lib(token="FZGu2fc2yuc6zyi1lqq1sqsIj9bJv0kkCXzzrwLvrRu7qwt4j6XeNCzEagXtuju
 import ipdb;ipdb.set_trace()
 twitter = lib.gkaww.twitter["@0.0.2"]
 
-user_timeline = twitter.get(
+result = twitter.get(
   config="{\"consumer_key\":\"ZtSUgVNL9vLVEvMhJZUJwqR7O\",\"consumer_secret\":\"I93nmt7E361jPRD0xw0Z53Ok2uSF7WzOJXcJDwEijUQlrlXOPh\",\"access_token\":\"1036312409151918080-bDBUwjQ2Obfn9GtHXeE8vfkUiuyjUM\",\"access_token_secret\": \"r5K7MU6rj76wICiFtaN96b7YSIME6eam9FpilDwylJbkY\"}", # (required)
   path="statuses/user_timeline"
 )
