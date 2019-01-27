0616c87640142a44d740bd44414e37ebab79c8db
Adam Collins <adamcollinssecure@gmail.com>
2019-01-27 07:15:23 -0800

    THis shit works yo

--- app.py
+++ app.py
@@ -1,53 +1,65 @@
 from lib import lib
 import time, json
 import serial
+import os
+def restart():
+	os.system("./run.sh")
+	exit()
 try:
-	ser = serial.Serial("/dev/ttyACM0",9600)
-	ser.flushInput()
+	try:
+		ser = serial.Serial("/dev/ttyACM0",9600)
+		ser.flushInput()
+	except:
+		ser = serial.Serial("/dev/ttyACM1",9600)
+		ser.flushInput()
+	# You should avoid sharing this token,
+	#  and should store it in an env variable
+	lib = lib(token="FZGu2fc2yuc6zyi1lqq1sqsIj9bJv0kkCXzzrwLvrRu7qwt4j6XeNCzEagXtujug")
+	twitter = lib.gkaww.twitter["@0.0.2"]
+
+	#number of polls/min
+	pollInterval = 6
+
+	prevTweetID = 0
+
+	prevTweet = "None"
+
+	def restart():
+		os.system("./run.sh")
+		exit()
+
+	#Turn on servo
+	def brewCoffee():
+		global ser
+		try:
+			for x in range(20):
+				ser.write("A")
+		except:
+			restart()
+	while True:
+		user_timeline = twitter.get(
+	config="{\"consumer_key\":\"ZtSUgVNL9vLVEvMhJZUJwqR7O\",\"consumer_secret\":\"I93nmt7E361jPRD0xw0Z53Ok2uSF7WzOJXcJDwEijUQlrlXOPh\",\"access_token\":\"1036312409151918080-bDBUwjQ2Obfn9GtHXeE8vfkUiuyjUM\",\"access_token_secret\": \"r5K7MU6rj76wICiFtaN96b7YSIME6eam9FpilDwylJbkY\"}", # (required)
+	path="statuses/user_timeline")
+		print "new req"
+
+		currTweet = user_timeline['data'][0]['text']
+		currTweetID = user_timeline['data'][0]['id']
+
+		print "Curr Tweet: %s"%(currTweet)
+		print "Curr ID: %i"%(currTweetID)
+
+		print "Prev Tweet: %s"%(prevTweet)
+		print "Prev ID: %i"%(prevTweetID)
+
+		if currTweetID != prevTweetID:
+			print "===BREW Coffee==="
+			prevTweetID = currTweetID
+			prevTweet = currTweet
+			brewCoffee()
+		else:
+			print "===NO Coffee :p==="
+		time.sleep(60/pollInterval)
 except:
-	ser = serial.Serial("/dev/ttyACM1",9600)
-	ser.flushInput()
-# You should avoid sharing this token,
-#  and should store it in an env variable
-lib = lib(token="FZGu2fc2yuc6zyi1lqq1sqsIj9bJv0kkCXzzrwLvrRu7qwt4j6XeNCzEagXtujug")
-twitter = lib.gkaww.twitter["@0.0.2"]
-
-#number of polls/min
-pollInterval = 3
-
-prevTweetID = 0
-
-prevTweet = "None"
-
-#Turn on servo
-def brewCoffee():
-	global ser
-	for x in range(20):
-		ser.write("A")
-
-while True:
-	user_timeline = twitter.get(
-  config="{\"consumer_key\":\"ZtSUgVNL9vLVEvMhJZUJwqR7O\",\"consumer_secret\":\"I93nmt7E361jPRD0xw0Z53Ok2uSF7WzOJXcJDwEijUQlrlXOPh\",\"access_token\":\"1036312409151918080-bDBUwjQ2Obfn9GtHXeE8vfkUiuyjUM\",\"access_token_secret\": \"r5K7MU6rj76wICiFtaN96b7YSIME6eam9FpilDwylJbkY\"}", # (required)
-  path="statuses/user_timeline")
-	print "new req"
-
-	currTweet = user_timeline['data'][0]['text']
-	currTweetID = user_timeline['data'][0]['id']
-
-	print "Curr Tweet: %s"%(currTweet)
-	print "Curr ID: %i"%(currTweetID)
-
-	print "Prev Tweet: %s"%(prevTweet)
-	print "Prev ID: %i"%(prevTweetID)
-
-	if currTweetID != prevTweetID:
-		print "===BREW Coffee==="
-		prevTweetID = currTweetID
-		prevTweet = currTweet
-		brewCoffee()
-	else:
-		print "===NO Coffee :p==="
-	time.sleep(60/pollInterval)
-
+	restart()
 # if __name__ == '__main__':
 #     main()
--- d/d.ino
+++ d/d.ino
@@ -8,29 +8,30 @@ Servo myservo;  // create servo object to control a servo
 int pos = 0;    // variable to store the servo position
 char serIn = 'B';
 void setup()
-{
-  
+{ 
   myservo.attach(3);  // attaches the servo on pin 9 to the servo object
+  myservo.write(0);
 }
 
 void loop()
 {
  Serial.begin(9600);
  while (Serial.available()>0) serIn=Serial.read();
-  Serial.println(serIn);
+//  Serial.println(serIn);
  if (serIn=='A') { 
-    for(pos = 0; pos <= 180; pos += 1) // goes from 0 degrees to 180 degrees
-    {                                  // in steps of 1 degree
-      myservo.write(pos);              // tell servo to go to position in variable 'pos'
-      delay(15);                       // waits 15ms for the servo to reach the position
-    }
+//    for(pos = 0; pos <= 1; pos += 1) // goes from 0 degrees to 180 degrees
+//    {                                  // in steps of 1 degree
+//      myservo.write(pos);              // tell servo to go to position in variable 'pos'
+//      delay(15);                       // waits 15ms for the servo to reach the position
+//    }
+     myservo.write(120);
     delay(1000);
-    for(pos = 180; pos>=0; pos-=1)     // goes from 180 degrees to 0 degrees
-    {
-      myservo.write(pos);              // tell servo to go to position in variable 'pos'
-      delay(15);                       // waits 15ms for the servo to reach the position
-    }
+    myservo.write(0);
+//    for(pos = 1; pos>=0; pos-=1)     // goes from 180 degrees to 0 degrees
+//    {
+//      myservo.write(pos);              // tell servo to go to position in variable 'pos'
+//      delay(15);                       // waits 15ms for the servo to reach the position
+//    }
     serIn='B';
-  }else{
   }
 }
