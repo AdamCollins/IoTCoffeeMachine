5f0578e95ef7c8171179f664911ae9af39980523
Grace Lam <gracesmile88@hotmail.com>
2019-01-26 18:51:14 -0800

    Working code for reading timeline Can pull data from timeline. Found bugs with stdlib we used.

--- app.py
+++ app.py
@@ -3,11 +3,10 @@ from lib import lib
 # You should avoid sharing this token,
 #  and should store it in an env variable
 lib = lib(token="FZGu2fc2yuc6zyi1lqq1sqsIj9bJv0kkCXzzrwLvrRu7qwt4j6XeNCzEagXtujug")
-
+import ipdb;ipdb.set_trace()
 twitter = lib.gkaww.twitter["@0.0.2"]
 
-result = twitter.statuses.user_timeline(
+user_timeline = twitter.get(
   config="{\"consumer_key\":\"ZtSUgVNL9vLVEvMhJZUJwqR7O\",\"consumer_secret\":\"I93nmt7E361jPRD0xw0Z53Ok2uSF7WzOJXcJDwEijUQlrlXOPh\",\"access_token\":\"1036312409151918080-bDBUwjQ2Obfn9GtHXeE8vfkUiuyjUM\",\"access_token_secret\": \"r5K7MU6rj76wICiFtaN96b7YSIME6eam9FpilDwylJbkY\"}", # (required)
-  screen_name="AdamsCoffeemak1",
-  since_id=0
+  path="statuses/user_timeline"
 )
